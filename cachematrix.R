756bb82a5353c25f5f88530181ea414caf1121d2
jose <jose.chez.morales@gmail.com>
2018-04-24 17:43:36 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## April-2018 version 1.0.
+## This code will cache the Inverse of a Matrix, it has 2 functions.
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+# Good practice if you use Camel cases as naming convention for the functions.
+makeCacheMatrix <- function(x = numeric()) {
+  
+  ## Initialize
+  ToCache <- NULL
+  
+  ## This function has 4 methods
+  
+  ## 1. Sets cached matrix
+  setM <- function(NewVal) {
+    x <<- NewVal
+    ToCache <<- NULL
+  }
+  
+  # 2. Get input
+  getM <- function() {
+    x
+  }
+  
+  # 3. Update 
+  SaveM <- function(data) {
+    ToCache <<- data
+  }
+  
+  # 4. Get cache
+  getCache <- function() {
+    ToCache
+  }
+  
+  # return a list. Each named element of the list is a function
+  ## a; Set the value of the matrix
+  ## b; Get the value of the matrix
+  ## c; Set the value of the inverse matrix
+  ## d; Get the value of the inverse matrix
+  list(setM = setM, getM = getM, SaveM = SaveM, getCache = getCache)
 }
 
 
-## Write a short comment describing this function
+## Return the inverse matrix of 'x'
+cacheSolve <- function(y, ...) {
+  ##  
+  inverse <- y$getCache()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if(!is.null(inverse)) {
+    print("Checking for cached data")
+    return(inverse)
+  }
+  data <- y$getM() ## Get Matrix
+  inverse <- solve(data) ## Inverse operation
+  y$SaveM(inverse) ## Produce cache inverse matrix
+  inverse
 }
